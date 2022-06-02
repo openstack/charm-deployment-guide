5920c9ffab2c73e07c9515951a1dff60c2c739d9
Peter Matulis <peter.matulis@canonical.com>
2022-06-02 13:33:40 -0400

    Add intersphinx Sphinx extension
    Add the intersphinx Sphinx extension to enable seamless
    linking from the charm-guide.
    
    This is already enabled for the charm-guide - for seamless
    linking from this guide (deploy-guide).
    
    Change-Id: I36442bfc8ff9267786cce87b32dfff2348c30f57

--- deploy-guide/source/conf.py
+++ deploy-guide/source/conf.py
@@ -36,7 +36,14 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # TODO(ajaeger): enable PDF building, for example add 'rst2pdf.pdfbuilder'
-extensions = ['openstackdocstheme']
+extensions = [
+    'openstackdocstheme',
+    'sphinx.ext.intersphinx'
+]
+
+intersphinx_mapping = {
+    'cg': ('https://docs.openstack.org/charm-guide/latest', None)
+}
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
